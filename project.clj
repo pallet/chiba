a566118ed4ae802ea839e1620cf2cf9c8c0bbf77
Hugo Duncan <hugo@hugoduncan.org>
2014-08-12 07:36:29 -0400

    Update to bultitude 0.2.6

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [bultitude "0.1.3"]])
+                 [bultitude "0.2.6"]])
