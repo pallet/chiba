77e2e24225eecd0fbec3f7b89d370264680ec312
Hugo Duncan <hugo@hugoduncan.org>
2014-08-12 07:47:29 -0400

    Add release plugin

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,9 @@
-{:dev {:plugins [[lein-set-version "0.3.0"]]}
- :doc {:dependencies [[codox-md "0.2.0"]]
-       :codox {:writer codox-md.writer/write-docs
-               :output-dir "doc/0.2/api"
-               :src-dir-uri "https://github.com/pallet/java-crate/blob/develop"
-               :src-linenum-anchor-prefix "L"}
-       :aliases {"marg" ["marg" "-d" "doc/0.2/annotated/"]
-                 "codox" ["doc"]
+{:doc {:dependencies [[codox-md "0.2.0"]],
+       :codox {:writer codox-md.writer/write-docs,
+               :output-dir "doc/0.2/api",
+               :src-dir-uri "https://github.com/pallet/chiba/blob/develop",
+               :src-linenum-anchor-prefix "L"},
+       :aliases {"marg" ["marg" "-d" "doc/0.2/annotated/"],
+                 "codox" ["doc"],
                  "doc" ["do" "codox," "marg"]}}
- :release
- {:set-version
-  {:updates [{:path "README.md" :no-snapshot true}]}}}
+ :dev {:plugins [[lein-pallet-release "RELEASE"]]}}
